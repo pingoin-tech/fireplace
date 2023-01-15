18043c0a8bd75e46d2d08d9f8a7837b0aefda47f
Pingoin <pdrente@googlemail.com>
2023-01-15 14:24:04 +0100

    feat: first websupport

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use chrono::Utc;
+use home_center::{devices::SENSOR_LIST, mqtt};
 use rumqttc::QoS;
-use std::{time::Duration, collections::HashMap};
+use std::{collections::HashMap, time::Duration};
 use tokio::{self, task, time};
-use home_center::{mqtt, devices::SENSOR_LIST};
-
 
 #[tokio::main]
 async fn main() {
     {
-        SENSOR_LIST.lock().expect("could not lock").get_or_insert(HashMap::new());
+        SENSOR_LIST
+            .lock()
+            .expect("could not lock")
+            .get_or_insert(HashMap::new());
     }
 
     let now = Utc::now();
@@ -20,7 +23,13 @@ async fn main() {
         .await
         .unwrap();
 
-    let mqtt_work_task = task::spawn(mqtt::work(eventloop));
+    task::spawn(mqtt::work(eventloop));
+
+    let http_handler = HttpServer::new(|| App::new().service(hello))
+        .bind(("0.0.0.0", 8080))
+        .unwrap()
+        .run();
+
     for i in 0..10 {
         client
             .publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize])
@@ -28,5 +37,19 @@ async fn main() {
             .unwrap();
         time::sleep(Duration::from_millis(100)).await;
     }
-    mqtt_work_task.await.unwrap();
+    //mqtt_work_task.await.unwrap();
+    http_handler.await.unwrap();
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    match SENSOR_LIST.lock() {
+        Ok(mut list_option) => {
+            if let Some(list) = list_option.as_mut() {
+                return HttpResponse::Ok().json(list);
+            }
+        }
+        Err(_) => {}
+    }
+    HttpResponse::Ok().body("bla")
 }
